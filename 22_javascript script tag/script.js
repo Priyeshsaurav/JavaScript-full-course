2642a7dd3a7b2bee610bbffaec23c74e2f547909
PRIYESH SAURAV <priyeshsaurav@PRIYESHs-MacBook-Air.local>
2024-12-10 22:58:39 +0530

    parents siblings of an element

--- 22_javascript script tag/script.js
+++ 22_javascript script tag/script.js
@@ -1,8 +1,14 @@
-console.log("hello world")
-obj = {
-    a:1,
-    b:2,
-    c:3
-}
-console.table(obj)
-console.info("hey! i am here")
+// console.log("hello world")
+// obj = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// console.table(obj)
+// console.info("hey! i am here")
+
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
