5e93848b06e04127e27fc22c6557bc612f6cd76c
PRIYESH SAURAV <priyeshsaurav@PRIYESHs-MacBook-Air.local>
2024-12-11 01:03:40 +0530

    element of a table

--- 23_Element of an navigation and table/script.js
+++ 23_Element of an navigation and table/script.js
@@ -1,7 +1,14 @@
-const changeBgRed =()=>{
-    document.body.firstElementChild.style.background ="red";
-}
+// const changeBgRed =()=>{
+//     document.body.firstElementChild.style.background ="red";
+// }
 
-let b = document.body
-console.log("first child of b is :",b.firstChild)
-console.log("first element child of b is :",b.firstElementChild)
+// let b = document.body
+// console.log("first child of b is :",b.firstChild)
+// console.log("first element child of b is :",b.firstElementChild)
+
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tFoot)
+console.log(t.tHead.firstElementChild.firstElementChild)
