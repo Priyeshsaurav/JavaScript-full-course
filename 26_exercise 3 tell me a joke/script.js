a90980b89bda2aa51aba2b5c09797980aae1eba4
PRIYESH SAURAV <priyeshsaurav@PRIYESHs-MacBook-Air.local>
2025-01-08 18:26:30 +0530

    tell me a joke

---
+++ 26_exercise 3 tell me a joke/script.js
@@ -0,0 +1,3 @@
+// element.innerHtml is used to populate a div with HTML search Online about this method and create a website with a div
+//  tag containning a random joke given an array of jokes use Math.random and fetch jokes from the internet (use any website
+//  to create the array) your website should show a random joke on every reload. Min length of your jokes array should be 10
