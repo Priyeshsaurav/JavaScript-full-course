3e1066b4d2aa2718015110acf75edd1cf7a76acb
PRIYESH SAURAV <priyeshsaurav@PRIYESHs-MacBook-Air.local>
2025-01-18 22:26:06 +0530

    EventListener

---
+++ 34_Add_event_listner/script.js
@@ -0,0 +1,18 @@
+let x = function(e){   
+    console.log(e.target)
+    console.log(e.target.value)   // here you can see (e) means ponterEvent and this is event object
+    alert('hello world1');
+}
+
+let y = function(e){
+    alert('hello world2')
+}
+
+btn.addEventListener('click',x);
+
+// btn.addEventListener('click',y); //Here we can dont use whole function() which is mention in row1 ,2,3 for add and remove event listener.
+
+// let b = prompt("enter a number")
+// if(b==2){
+//     btn.removeEventListener('click',x)
+// }
