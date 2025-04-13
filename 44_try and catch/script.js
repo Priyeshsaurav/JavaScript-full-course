3ef8b043f31f62c3b546de11430eff428e0f5862
PRIYESH SAURAV <priyeshsaurav@PRIYESHs-MacBook-Air.local>
2025-04-14 04:33:16 +0530

    try and catch

---
+++ 44_try and catch/script.js
@@ -0,0 +1,22 @@
+setTimeout(()=>{
+    console.log("Hacking wifi... please wait...")
+},3000)
+try{
+    console.log(rahul)
+}
+catch(error){
+    console.log("error")
+}
+
+setTimeout(()=>{
+    console.log("Fetching username and password...... please wait")
+    
+},4000)
+
+setTimeout(()=>{
+    console.log("hacking rahul fb id..... please wait")
+},5000)
+
+setTimeout(()=>{
+    console.log("username and password of rahul fetched.... please wait")
+},7000)
